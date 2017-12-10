d954c64d3058952c4d9fd68d0b26489ed15e4046
Aaron Myers <lakeoftea@gmail.com>
2017-12-09 20:52:10 -0600

    Added jwt tests

--- src/main/groovy/util/JwtUtil.groovy
+++ src/main/groovy/util/JwtUtil.groovy
@@ -1,4 +1,43 @@
 package util
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTCreationException
+import com.auth0.jwt.exceptions.JWTVerificationException
+import com.auth0.jwt.interfaces.DecodedJWT
+import org.apache.commons.lang3.time.DateUtils
+
 class JwtUtil {
+
+    static String generateToken() {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(ConfigUtil.getConfig().secretKey)
+            return JWT.create().sign(algorithm)
+        } catch (JWTCreationException ex){
+            println ex.getStackTrace()
+        }
+    }
+
+    static String generateToken(int minutes) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(ConfigUtil.getConfig().secretKey)
+            JWT.create().withExpiresAt(DateUtils.addMinutes(new Date(), 5)).sign(algorithm)
+        } catch (JWTCreationException ex){
+            println ex.getStackTrace()
+        }
+    }
+
+    static boolean verifyToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256(ConfigUtil.getConfig().secretKey)
+        JWTVerifier verifier = JWT.require(algorithm).build()
+        try {
+            verifier.verify(token).getToken()
+            return true
+        } catch (JWTVerificationException|UnsupportedEncodingException ex){
+            println ex.getStackTrace()
+            return false
+        }
+    }
+
 }
--- src/test/groovy/util/JwtUtilTest.groovy
+++ src/test/groovy/util/JwtUtilTest.groovy
@@ -1,4 +1,27 @@
 package util
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
+
 class JwtUtilTest {
+
+    @Test
+    void canJwtUtilGenerateToken(){
+        assertEquals JwtUtil.generateToken().getClass(), new String().getClass()
+        assertNotNull JwtUtil.generateToken()
+        assertNotNull JwtUtil.generateToken(5)
+    }
+
+    @Test
+    void isDoesVerifyTokenAcceptValidTokensAndRejectExpiredTokens(){
+        Algorithm algorithm = Algorithm.HMAC256("test")
+        assertTrue JwtUtil.verifyToken(JwtUtil.generateToken(5))
+        assertFalse JwtUtil.verifyToken(JWT.create().withExpiresAt(new Date(new Date().getTime() - 10)).sign(algorithm))
+    }
 }
